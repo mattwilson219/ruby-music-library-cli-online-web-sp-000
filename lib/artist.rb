f768213e8aace7a8d7330ed5fc9d751286a42172
Matthew Wilson <wilsonmatt219@gmail.com>
2019-06-17 17:46:55 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+
+attr_accessor :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+end
+
+def self.all
+  @@all
+end
+
+def self.destroy_all
+  @@all = []
+end
+
+def save
+  @@all << self
+end
+
+def self.create(name)
+  artist = self.new(name)
+  artist.save
+  artist
+  end
+end
