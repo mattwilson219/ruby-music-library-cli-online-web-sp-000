c96082af5b7159fb5a9d0543b326ccf5a3d03ffc
Matthew Wilson <wilsonmatt219@gmail.com>
2019-06-19 19:09:36 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -28,8 +28,8 @@ class MusicLibraryController
   end
 
   def list_artists
-    Artist.all.sort_by(&:name).each.with_index(1) do |artist, index|
-      puts"#{index}. #{artist.name}"
+    Artist.all.sort_by(&:name).each.with_index(1) do |ar, index|
+      puts "#{index}. #{ar.name}"
     end
   end
 
