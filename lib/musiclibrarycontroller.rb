abfb7f3a9a5b29e1dcf3261012180f80b112ec28
Matthew Wilson <wilsonmatt219@gmail.com>
2019-06-19 19:13:10 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -1,42 +1,17 @@
 class MusicLibraryController
-
-  def initialize(path = "./db/mp3s")
-    MusicImporter.new(path).import
-  end
-
-  def call
-    input = ''
-  while input != 'exit'
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
-
-    input = gets.strip
-   end
-  end
-
-  def list_songs
-    Song.all.sort_by(&:name).each.with_index(1) do |song, index|
-      puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    def initialize(path='./db/mp3s')
+        MusicImporter.new(path).import
     end
-  end
 
-  def list_artists
-    artists = Artist.all.sort {|a,b| a.name <=> b.name}
-     artists.each.with_index(1) {|artist, i| puts "#{i}. #{artist.name}"}
-
-  end
-
-  def list_genres
-    Genre.all.sort_by(&:name).each.with_index(1) do |genre, index|
-      puts "#{index}. #{genre.name}"
+     def call
+        input = ""
+        until input == "exit"
+            input = gets.chomp
+            case input
+            when exit
+                puts "Goodbye."
+            end     
+        end
     end
-  end
 
-end
+ end 
